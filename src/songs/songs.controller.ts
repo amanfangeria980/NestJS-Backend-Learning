e8ce1b96d5552fed4aac96ec9c1c45e34c4707a0
Aman Fangeria <amanfangeria980@gmail.com>
2025-02-03 01:14:11 +0530

    feat: defined 5 endpoints

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('songs')
 export class SongsController {
   @Get()
   findAll() {
-    return 'Get All Messages';
+    return 'Get All Songs endpoint';
+  }
+  @Get(':id')
+  findOne() {
+    return 'Get one song by id endpoint';
+  }
+  @Put(':id')
+  update() {
+    return 'Update song by id endpoint';
+  }
+  @Delete(':id')
+  delete() {
+    return 'Delete a song by id endpoint';
+  }
+  @Post()
+  create() {
+    return 'add a new song';
   }
 }
